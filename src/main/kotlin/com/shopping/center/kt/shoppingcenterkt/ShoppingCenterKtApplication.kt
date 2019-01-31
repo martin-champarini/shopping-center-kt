40758863c52975487d92c396f9027406469de8dd
Martin Champarini <martin.champarini.dev@gmail.com>
2019-01-30 21:45:47 -0300

    first commit

---
+++ src/main/kotlin/com/shopping/center/kt/shoppingcenterkt/ShoppingCenterKtApplication.kt
@@ -0,0 +1,12 @@
+package com.shopping.center.kt.shoppingcenterkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingCenterKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoppingCenterKtApplication>(*args)
+}
+
