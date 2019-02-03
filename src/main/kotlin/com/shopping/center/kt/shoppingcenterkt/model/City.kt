5b586a3427ce3bd7c39b2f45ce0a0bfeeac6c023
Martin Champarini <martin.champarini.dev@gmail.com>
2019-02-03 13:56:18 -0300

    Added datasource config

--- src/main/kotlin/com/shopping/center/kt/shoppingcenterkt/model/City.kt
+++ src/main/kotlin/com/shopping/center/kt/shoppingcenterkt/model/City.kt
@@ -2,8 +2,10 @@ package com.shopping.center.kt.shoppingcenterkt.model
 
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
+@Table(name = "`CITY`")
 data class City(
         @Id
         var name: String,
