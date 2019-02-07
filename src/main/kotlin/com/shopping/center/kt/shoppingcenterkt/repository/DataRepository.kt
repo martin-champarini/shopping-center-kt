bbf014d0a97fae552706bb8d33b91a571a4d0c83
Martin Champarini <martin.champarini.dev@gmail.com>
2019-02-06 22:13:46 -0300

    Added Repositories

--- src/main/kotlin/com/shopping/center/kt/shoppingcenterkt/repository/DataRepository.kt
+++ src/main/kotlin/com/shopping/center/kt/shoppingcenterkt/repository/DataRepository.kt
@@ -2,8 +2,23 @@ package com.shopping.center.kt.shoppingcenterkt.repository
 
 import com.shopping.center.kt.shoppingcenterkt.model.City
 import com.shopping.center.kt.shoppingcenterkt.model.Country
+import com.shopping.center.kt.shoppingcenterkt.model.Order
+import com.shopping.center.kt.shoppingcenterkt.model.Product
+import com.shopping.center.kt.shoppingcenterkt.model.StatusOrder
+import com.shopping.center.kt.shoppingcenterkt.model.User
+import com.shopping.center.kt.shoppingcenterkt.model.UserSession
 import org.springframework.data.repository.CrudRepository
 
-interface CityRepository:CrudRepository<City,String>
+interface CityRepository:CrudRepository<City, String>
 
-interface CountryRepository:CrudRepository<Country,String>
+interface CountryRepository:CrudRepository<Country, String>
+
+interface OrderRepository:CrudRepository<Order, Long>
+
+interface ProductRepository:CrudRepository<Product, Long>
+
+interface StatusOrderRepository:CrudRepository<StatusOrder, String>
+
+interface UserSessionRepository:CrudRepository<UserSession, String>
+
+interface UserRepository:CrudRepository<User, String>
---
+++ src/main/kotlin/com/shopping/center/kt/shoppingcenterkt/service/OrderService.kt
@@ -0,0 +1,8 @@
+package com.shopping.center.kt.shoppingcenterkt.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class OrderService() {
+
+}
